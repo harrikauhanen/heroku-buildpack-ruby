7f7bb2981a269a101da1ac023407b7911d6f8600
puneet <puneet@bigbinary.com>
2017-01-01 17:19:19 +0530

    Adding webpacker compile task

--- lib/language_pack/helpers/yarn_installer.rb
+++ lib/language_pack/helpers/yarn_installer.rb
@@ -31,6 +31,7 @@ class LanguagePack::YarnInstaller
   def install_node
     @node_fetcher.fetch_untar("", "node-v6.9.1-linux-x64/bin")
     FileUtils.cp_r("node-v6.9.1-linux-x64/bin/", ".")
+    FileUtils.rm("npm")
     FileUtils.rm_rf("node-v6.9.1-linux-x64")
   end
 
