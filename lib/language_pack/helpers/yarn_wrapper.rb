659166dc9dd004ffd351c26354fa15281c549de0
puneet <puneet@bigbinary.com>
2017-01-03 12:21:15 +0530

    #2

--- lib/language_pack/helpers/yarn_wrapper.rb
+++ lib/language_pack/helpers/yarn_wrapper.rb
@@ -4,22 +4,13 @@ require 'uri'
 
 class LanguagePack::Helpers::YarnWrapper
   include LanguagePack::ShellHelpers
-
-  class PackageFileParseError < BuildpackError
-    def initialize(error)
-      msg = "There was an error parsing your package.json, we cannot continue\n"
-      msg << error
-      super msg
-    end
-  end
+  include YarnConfigFileHelper
 
   VENDOR_DIR_PATH      = './vendor'
 
   def initialize
-    @vendor_dir_path =  ENV['VENDOR_DIR_PATH'] || VENDOR_DIR_PATH
-    @vendor_path = Pathname.new(@vendor_dir_path).realpath
-    @yarn_fetcher = LanguagePack::Fetcher.new("https://yarnpkg.com/downloads/")
-    @node_fetcher = LanguagePack::Fetcher.new("https://nodejs.org/dist/")
+    vendor_dir_path =  ENV['VENDOR_DIR_PATH'] || VENDOR_DIR_PATH
+    @vendor_path = Pathname.new(vendor_dir_path).realpath
   end
 
   def install_node_modules_and_dependencies
@@ -35,76 +26,126 @@ class LanguagePack::Helpers::YarnWrapper
 
   def node_app?
     FileUtils.chdir @vendor_path do
-      return yarn_lock_file_path && yarn_lock_file_path
+      return yarn_lock_file && package_file
     end
   end
 
   private
 
   def install_yarn
-    topic "installing yarn #{yarn_version}"
-    @yarn_fetcher.fetch_untar("#{yarn_version}/yarn-v#{yarn_version}.tar.gz", "dist/")
-    FileUtils.cp_r("dist/.", "./yarn")
-    FileUtils.rm_rf("dist")
+    YarnInstaller.new.perform
   end
 
   def install_node
-    untar_dir = "node-v#{node_version}-linux-x64"
-    topic "installing #{untar_dir}"
-    @node_fetcher.fetch_untar("v#{node_version}/#{untar_dir}.tar.gz", "#{untar_dir}/bin")
-    FileUtils.mkdir("./node")
-    FileUtils.cp_r("#{untar_dir}/.", "./node")
-    FileUtils.rm_rf(untar_dir)
+    NodeInstaller.new.perform
   end
 
   def install_packages
-    topic "installing node modules"
     run! "../bin/yarn install"
-    topic "installing node packages done"
   end
 
-  def yarn_version
-    @yarn_version ||= open("https://semver.herokuapp.com/yarn/resolve/#{provided_yarn_version}")
+  class VersionResolver
+
+    def resolve_node(provided_version)
+      open("https://semver.herokuapp.com/node/resolve/#{provided_version}")
+    end
+
+    def resolve_yarn(provided_version)
+      open("https://semver.herokuapp.com/yarn/resolve/#{provided_version}")
+    end
+
+    private
+
+    def open uri
+      super(URI.escape(uri)).read
+    end
+
   end
 
-  def node_version
-    @node_version ||= open("https://semver.herokuapp.com/node/resolve/#{provided_node_version}")
-  end
+  module YarnConfigFileHelper
 
-  def parsed_package_file
-    @parsed_package_file ||= begin
-      json = File.read(package_dot_json_file_path)
-      JSON.parse(json)
+    def parsed_package_file
+      @parsed_package_file ||= begin
+        json = File.read(package_file)
+        JSON.parse(json)
+      end
+    end
+
+    def engine_config
+      @engine_config ||= parsed_package_file.fetch('engines', {})
+    end
+
+    def node_version
+      engine_config['node']
+    end
+
+    def yarn_version
+      engine_config['yarn']
+    end
+
+    def yarn_lock_file
+      Dir["yarn.lock"].first
+    end
+
+    def package_file
+      Dir["package.json"].first
     end
   end
 
-  def engine_config
-    @engine_config ||= parsed_package_file.fetch('engines', {})
+  class NodeInstaller
+    include YarnConfigFileHelper
+
+    def initialize
+      @node_fetcher = LanguagePack::Fetcher.new("https://nodejs.org/dist/")
+    end
+
+    def perform
+      topic "installing #{binary_name}"
+      @node_fetcher.fetch_untar(binary_path, "#{binary_name}/bin")
+      FileUtils.cp_r("#{binary_name}/.", "./node")
+      FileUtils.rm_rf(binary_name)
+    end
+
+    private
+
+    def version
+      @version ||= VersionResolver.new.resolve_node(node_version)
+    end
+
+    def binary_name
+      @binary_name ||= "node-v#{version}-linux-x64"
+    end
+
+    def binary_path
+      "v#{version}/#{binary_name}.tar.gz"
+    end
+
   end
 
-  def provided_node_version
-    engine_config['node']
-  end
+  class YarnInstaller
+    include YarnConfigFileHelper
 
-  def provided_yarn_version
-    engine_config['yarn']
-  end
+    def initialize
+      @yarn_fetcher = LanguagePack::Fetcher.new("https://yarnpkg.com/downloads/")
+    end
 
-  def yarn_lock_file_path
-    Dir["yarn.lock"].first
-  end
+    def perform
+      topic "installing yarn v#{version}"
+      @yarn_fetcher.fetch_untar(binary_path, "dist/")
+      FileUtils.cp_r("dist/.", "./yarn")
+      FileUtils.rm_rf("dist")
+    end
 
-  def package_dot_json_file_path
-    Dir["package.json"].first
-  end
+    private
 
-  def instrument title
-    topic title
-    yield
-  end
+    def version
+      @version ||= VersionResolver.new.resolve_yarn(yarn_version)
+    end
 
-  def open uri
-    super(URI.escape(uri)).read
+    def binary_path
+      "#{version}/yarn-v#{version}.tar.gz"
+    end
+
   end
 
 end
